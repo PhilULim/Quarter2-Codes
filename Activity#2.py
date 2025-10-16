28b5e0519ac2e8a93f4c3fffe6892af58f8485d6
PhilULim <philjustin.lim@smc.pshs.edu.ph>
2025-10-16 13:40:26 +0800

    Activity #2
    This code asks the user for their age and shows the sum of every number starting from 1 to their age.

---
+++ Activity#2.py
@@ -0,0 +1,12 @@
+
+
+
+
+user_age =  (int(input("Hi There! Please input your age:")))
+total = 0
+for numbers in range(1,user_age+1, 1):
+    total += numbers
+print ("The sum of all the numbers from 1 to",user_age, "is", total)
+
+
+
